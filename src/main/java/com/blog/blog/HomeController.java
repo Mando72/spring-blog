b22a9d8f5b991e839e3fb87744a64d5c714bcefa
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-09-07 14:43:00 -0500

    exercises completed

--- src/main/java/com/blog/blog/HomeController.java
+++ src/main/java/com/blog/blog/HomeController.java
@@ -1,15 +1,18 @@
 package com.blog.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage(){
-        return "This is the Landing Page!";
+    @GetMapping("/hello/{name}")
+    public String returnHomePage(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        model.addAttribute("mando", "mando");
+         return "home";
     }
 }
--- src/main/java/com/blog/blog/PostController.java
+++ src/main/java/com/blog/blog/PostController.java
@@ -1,41 +1,60 @@
-package com.blog.blog;
+package com.blog.blog.controllers;
 
+import com.blog.blog.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping(path = "/post/")
-    @ResponseBody
+    List<Post> posts = new ArrayList<>();
 
-    public String poInd(){
-        return "Showing all post";
+//    method	url	description
+//    GET	/posts	posts index page
+//    GET	/posts/{id}	view an individual post
+//    GET	/posts/create	view the form for creating a post
+//    POST	/posts/create	create a new post
+
+    public PostController(){
+
+        posts.add(new Post("My family","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam corporis dignissimos dolore exercitationem impedit, iure non nulla optio saepe sint soluta tenetur velit vero, voluptatem! Animi dignissimos eveniet incidunt?"));
+        posts.add(new Post("My feelings","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam corporis dignissimos non nulla optio saepe sint soluta tenetur velit vero, voluptatem! Animi dignissimos eveniet incidunt?"));
+        posts.add(new Post("My house","Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam corporis dignissimos dolore exercitationem "));
+
     }
 
-    @GetMapping(path = "/post/{id}")
-    @ResponseBody
+    @GetMapping("/posts")
+    public String index(Model viewModel) {
 
-    public String poInv(@PathVariable long id){
-        return "Showing posy with id of "+ id;
+        viewModel.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
-    @GetMapping(path = "/posts/create")
-    @ResponseBody
-
-    public String createForm(){
-        return "Show form to create post";
+    @GetMapping("/posts/{id}")
+    public String show(@PathVariable long id, Model viewModel) {
+        Post post = posts.get( (int) id - 1);
+        viewModel.addAttribute("post", post);
+        return "posts/show";
     }
 
-    @PostMapping(path = "/posts/create")
+    @GetMapping("/posts/create")
     @ResponseBody
+    public String show() {
+        return "Showing form to create new post!";
+    }
 
-    public String newPos(){
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String create() {
         return "New post was created!";
     }
-
-
-
-
 
 }
